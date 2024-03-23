9ab1450f94ac96b279d71f8e253c33e37247c1fb
zwsong <1072860370@qq.com>
2024-03-23 16:20:35 +0800

    提交fastswap_rdma.h

---
+++ ping_pong/fastswap_rdma.h
@@ -0,0 +1,82 @@
+#if !defined(_SSWAP_RDMA_H)
+#define _SSWAP_RDMA_H
+
+#include <rdma/ib_verbs.h>
+#include <rdma/rdma_cm.h>
+#include <linux/inet.h>
+#include <linux/module.h>
+#include <linux/list.h>
+#include <linux/mm_types.h>
+#include <linux/gfp.h>
+#include <linux/pagemap.h>
+#include <linux/spinlock.h>
+
+
+
+enum qp_type {
+  QP_READ_SYNC,
+  QP_READ_ASYNC,
+  QP_WRITE_SYNC
+};
+
+struct sswap_rdma_dev {
+  struct ib_device *dev;
+  struct ib_pd *pd;
+};
+
+struct rdma_req {
+  struct completion done;
+  struct list_head list;
+  struct ib_cqe cqe;
+  u64 dma;
+  struct page *page;
+  int len;//+++
+  void *src;//+++
+};
+
+struct sswap_rdma_ctrl;
+
+struct rdma_queue {
+  struct ib_qp *qp;
+  struct ib_cq *cq;
+  spinlock_t cq_lock;
+  enum qp_type qp_type;
+
+  struct sswap_rdma_ctrl *ctrl;
+
+  struct rdma_cm_id *cm_id;
+  int cm_error;
+  struct completion cm_done;
+
+  atomic_t pending;
+};
+
+struct sswap_rdma_memregion {
+    u64 baseaddr;
+    u32 key;
+};
+
+struct sswap_rdma_ctrl {
+  struct sswap_rdma_dev *rdev; // TODO: move this to queue
+  struct rdma_queue *queues;
+  struct sswap_rdma_memregion servermr;
+
+  union {
+    struct sockaddr addr;
+    struct sockaddr_in addr_in;
+  };//2选1 sockaddr或sockaddr_in类型
+
+  union {
+    struct sockaddr srcaddr;
+    struct sockaddr_in srcaddr_in;
+  };
+};
+
+struct rdma_queue *sswap_rdma_get_queue(unsigned int idx, enum qp_type type);//c程序顺序执行 函数声明放在头文件 调用不用考虑定义顺序
+enum qp_type get_queue_type(unsigned int idx);
+int sswap_rdma_read_async(struct page *page, u64 roffset);
+int sswap_rdma_read_sync(struct page *page, u64 roffset);
+int sswap_rdma_write(struct page *page, u64 roffset);
+int sswap_rdma_poll_load(int cpu);
+
+#endif
